86ba91673f3ab3efff07b39524264cc04a0c0438
Eduardo Carneiro <eduardo.carneiro2907@gmail.com>
2022-07-02 13:52:33 -0300

    Add function that picks the most often week day

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -46,6 +46,10 @@ def get_peak_hour(hours_list)
   hours_list.max_by { |hour| hours_list.count(hour) }
 end
 
+def get_often_wday(wday_list)
+  wday_list.max_by { |wday| wday_list.count(wday)}
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -57,13 +61,13 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
-hours_list = []
+date_list = []
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone_number = validate_phone_number(row[:homephone])
-  hours_list <<  Time.strptime(row[:regdate], '%Y/%d/%m %H:%M').hour
+  date_list <<  Time.strptime(row[:regdate], '%Y/%d/%m %H:%M')
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
@@ -71,4 +75,5 @@ contents.each do |row|
   save_thank_you_letter(id,form_letter)
 end
 
-get_peak_hour(hours_list)
+get_peak_hour(date_list.map(&:hour))
+get_often_wday(date_list.map(&:wday))
